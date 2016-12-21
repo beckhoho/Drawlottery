2cc51e120789541c7ce7a8e6f29b933565ba0869
hdwx <346905702@qq.com>
2016-12-21 17:48:48 +0800

    添加服务层

---
+++ Api/src/main/java/com/hudongwx/drawlottery/mobile/service/user/IApproveSerivce.java
@@ -0,0 +1,29 @@
+package com.hudongwx.drawlottery.mobile.service.user;
+
+import com.hudongwx.drawlottery.mobile.entitys.Approve;
+import com.hudongwx.drawlottery.mobile.entitys.User;
+
+/**
+ * 开发公司：hudongwx.com<br/>
+ * 版权：294786949@qq.com<br/>
+ * <p>
+ *
+ * @author Kiter
+ * @version 1.0, 2016/12/21 <br/>
+ * @desc <p>
+ * <p>
+ * 创建　kiter　2016/12/21 16:51　<br/>
+ * <p>
+ *  认证信息Service
+ * <p>
+ * @email 294786949@qq.com
+ */
+public interface IApproveSerivce {
+
+    //添加用户认证信息
+    boolean addApproveMassage(Approve approve);
+
+    //查看用户认证信息
+    Approve select(User userAccount);
+
+}
---
+++ Api/src/main/java/com/hudongwx/drawlottery/mobile/service/user/impl/ApproveServiceImpl.java
@@ -0,0 +1,33 @@
+package com.hudongwx.drawlottery.mobile.service.user.impl;
+
+import com.hudongwx.drawlottery.mobile.entitys.Approve;
+import com.hudongwx.drawlottery.mobile.entitys.User;
+import com.hudongwx.drawlottery.mobile.service.user.IApproveSerivce;
+
+/**
+ * 开发公司：hudongwx.com<br/>
+ * 版权：294786949@qq.com<br/>
+ * <p>
+ *
+ * @author Kiter
+ * @version 1.0, 2016/12/21 <br/>
+ * @desc <p>
+ * <p>
+ * 创建　kiter　2016/12/21 17:06　<br/>
+ * <p>
+ *   认证信息service实现类
+ * <p>
+ * @email 294786949@qq.com
+ */
+public class ApproveServiceImpl implements IApproveSerivce{
+
+    @Override
+    public boolean addApproveMassage(Approve approve) {
+        return false;
+    }
+
+    @Override
+    public Approve select(User userAccount) {
+        return null;
+    }
+}
