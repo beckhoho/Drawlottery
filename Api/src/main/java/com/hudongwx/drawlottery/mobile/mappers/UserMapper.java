3cdc4160ff0e1cc37d9a0eaa9838bde83cfa4a15
origin_zy <enginezy@gmail.com>
2016-12-22 17:40:59 +0800

    修改实体类

--- Api/src/main/java/com/hudongwx/drawlottery/mobile/mappers/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hudongwx.drawlottery.mobile.mappers;
-
-import com.hudongwx.drawlottery.mobile.commn.BaseMapper;
-import com.hudongwx.drawlottery.mobile.entitys.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
