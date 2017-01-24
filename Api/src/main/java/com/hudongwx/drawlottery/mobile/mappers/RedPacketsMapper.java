6b4c6745f0ddfc143dd7ffc743a8e701679cdb68
wkq1990 <136226160@qq.com>
2017-01-24 14:22:32 +0800

    Merge branch 'master' of https://git.oschina.net/hudongwx/Drawlottery
    # Conflicts:
    #	Api/src/main/java/com/hudongwx/drawlottery/mobile/service/order/impl/OrdersServiceImplAsync.java

--- Api/src/main/java/com/hudongwx/drawlottery/mobile/mappers/RedPacketsMapper.java
+++ Api/src/main/java/com/hudongwx/drawlottery/mobile/mappers/RedPacketsMapper.java
@@ -11,4 +11,6 @@ public interface RedPacketsMapper extends BaseMapper<RedPackets> {
     List<RedPackets> selectByAccount(@Param("accountId")Long accountId);
 
     List<RedPackets> selectByState(@Param("accountId")Long accountId,@Param("state")Integer state);
+
+    List<RedPackets> selectUserUsableRedPackets(@Param("accountId")Long accountId, @Param("nowTime")Long nowTime);
 }
